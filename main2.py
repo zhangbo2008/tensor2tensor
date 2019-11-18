65d89f3d26621d23a6f455865140c278d8a01940
zhangbo2008 <15122306087@163.com>
2019-11-18 23:41:38 +0800

    12

--- main2.py
+++ main2.py
@@ -2,17 +2,25 @@
 自己下载数据集来跑.
 
 '''
+#encoding:utf-8
 
 import tensorflow as tf
 import os
 
-DATA_DIR = os.path.expanduser("d:/data/t2t/data")  # This folder contain the data
-TMP_DIR = os.path.expanduser("d:/data/t2t/tmp")
-TRAIN_DIR = os.path.expanduser("d:/data/t2t/train")  # This folder contain the model
-EXPORT_DIR = os.path.expanduser("d:/data/t2t/export")  # This folder contain the exported model for production
-TRANSLATIONS_DIR = os.path.expanduser("d:/data/t2t/translation")  # This folder contain  all translated sequence
-EVENT_DIR = os.path.expanduser("d:/data/t2t/event")  # Test the BLEU score
-USR_DIR = os.path.expanduser("d:/data/t2t/user")  # This folder contains our data that we want to add
+DATA_DIR = os.path.expanduser("/data/t2t/data")  # This folder contain the data
+TMP_DIR = os.path.expanduser("/data/t2t/tmp")
+
+
+from tensor2tensor.utils import registry
+from tensor2tensor import problems
+
+
+
+TRAIN_DIR = os.path.expanduser("/data/t2t/train")  # This folder contain the model
+EXPORT_DIR = os.path.expanduser("/data/t2t/export")  # This folder contain the exported model for production
+TRANSLATIONS_DIR = os.path.expanduser("/data/t2t/translation")  # This folder contain  all translated sequence
+EVENT_DIR = os.path.expanduser("/data/t2t/event")  # Test the BLEU score
+USR_DIR = os.path.expanduser("/data/t2t/user")  # This folder contains our data that we want to add
 
 tf.gfile.MakeDirs(DATA_DIR)
 tf.gfile.MakeDirs(TMP_DIR)
@@ -24,22 +32,16 @@ tf.gfile.MakeDirs(USR_DIR)
 
 
 
-PROBLEM = "translate_enfr_wmt32k" # We chose a problem translation English to French with 32.768 vocabulary
+PROBLEM = "translate_enzh_wmt8k" # We chose a problem translation English to French with 32.768 vocabulary
 MODEL = "transformer" # Our model
 HPARAMS = "transformer_big" # Hyperparameters for the model by default
                             # If you have a one gpu, use transformer_big_single_gpu
 
 
-
 from tensor2tensor.utils import registry
 from tensor2tensor import problems
 
-tmp1=problems.available() #Show all problems
-tmp2=registry.list_models() #Show all registered models
-print(tmp1)
-print(tmp2)
-
+print(problems.available()) #Show all problems
 
 t2t_problem = problems.problem(PROBLEM)
-t2t_problem.generate_data(DATA_DIR, TMP_DIR) #下不动,用debug进去看代码里面地址.
-
+t2t_problem.generate_data(DATA_DIR, TMP_DIR)
